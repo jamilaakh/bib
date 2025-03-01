7f6de635679cad088ed5221bfa32bc2b0bbc1866
jij <jakhezane@gmail.com>
2025-03-01 13:47:43 +0000

    Initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -55,6 +54,7 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
     protected static function boot()
     {
         parent::boot();
